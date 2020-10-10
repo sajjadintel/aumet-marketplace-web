efaabb15f16d3cc0c9ea77e17950c2415b4457a0
Sajad Abbasi <sajjadintel@gmail.com>
2020-10-10 20:45:07 +0400

    Fixed with APM-35

---
+++ ui/app/sale/orders/view-modal.php
@@ -0,0 +1,23 @@
+<!-- Modal-->
+<div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form method="POST" action="/" id="viewModalForm">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="viewModalTitle">Modal Title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i aria-hidden="true" class="ki ki-close"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p class="modal-text" id="viewModalText"></p>
+                </div>
+                <div class="modal-footer">
+                    <div class="modal-add-button">
+                        <button type="button" class="btn btn-primary font-weight-bold" id="viewModalAction">Save changes</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
