c1e3fd52a63823ff8071175e0d15424add2cc55f
Sajad Abbasi <sajjadintel@gmail.com>
2020-10-21 03:50:20 +0400

    Added Bonus List in Edit Quantity modal, Products page

--- assets/js/distributor-products.js
+++ assets/js/distributor-products.js
@@ -1,7 +1,5 @@
 'use strict';
 // Class definition
-var $repeater;
-var tempValue;
 var DistributorProductsDataTable = (function () {
 	// Private functions
 
@@ -312,17 +310,6 @@ var DistributorProductsDataTable = (function () {
 		$("label[for='editQuantityBonusDelete']").text(WebAppLocals.getMessage('delete'));
 		$("label[for='editQuantityBonusAdd']").text(WebAppLocals.getMessage('add'));
 
-		$repeater = $('#editQuantityBonusListRepeater').repeater({
-			isFirstItemUndeletable: true,
-			show: function () {
-				$(this).slideDown();
-			},
-			hide: function (deleteElement) {
-				if (confirm('Are you sure you want to delete this element?')) {
-					$(this).slideUp(deleteElement);
-				}
-			},
-		});
 		$repeater.setList(webResponse.data.bonus);
 
 		$("label[for='editQuantityBonusType']").text(WebAppLocals.getMessage('bonus'));
