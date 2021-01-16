5b9915ecbd2aba5bb29825c3d50f8389f6b3bd04
Sajad Abbasi <sajjadintel@gmail.com>
2021-01-16 06:00:02 +0330

    worked on MPW-69 / MPW-8 updated

--- ui/app/products/search/search.php
+++ ui/app/products/search/search.php
@@ -134,23 +134,6 @@ function compress_htmlcode($codedata)
         </div>
     </div>
     <!--end::Container-->
-    <style>
-			/* Chrome, Safari, Edge, Opera */
-			input::-webkit-outer-spin-button,
-			input::-webkit-inner-spin-button {
-				-webkit-appearance: none;
-				margin: 0;
-			}
-
-			/* Firefox */
-			input[type=number] {
-				-moz-appearance: textfield;
-			}
-
-			.quantityFreeInput:invalid + .quantityFreeHolder {
-				display: none;
-			}
-    </style>
     <script>
         var PageClass = function () {
             var elementId = "#datatable";
