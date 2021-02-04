7b3ceebb3b6ae904f832f0be8172cc7b17fae8bc
Sajad Abbasi <sajjadintel@gmail.com>
2021-02-04 18:56:23 +0500

    Unset session variable once error popup display

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -6,6 +6,7 @@ class DashboardController extends Controller
     function get()
     {
         $this->f3->set('notAuthorized',$this->f3->get('SESSION.notAuthorized'));
+        $this->f3->clear('SESSION.notAuthorized');
         if (!$this->f3->ajax()) {
             $this->f3->set("pageURL", "/web");
             echo View::instance()->render('app/layout/layout.php');
