dfbb39b7f0a120e5cfe67664840fc499a58c2e66
Sajad Abbasi <sajjadintel@gmail.com>
2021-02-04 04:40:36 +0330

    Worked on MPW-264 fixed

--- ui/app/products/single/entityProduct.php
+++ ui/app/products/single/entityProduct.php
@@ -133,6 +133,12 @@ function compress_htmlcode($codedata)
 
                             </div>
 
+                            <div>
+                                <span class="product-description">
+                                    asdfjlasd flksad fjlsakd fjlsadk fjasld fjaskd fjlsa dfjlsdak flkas fjlsa df
+                                    <?php echo $objEntityProduct->description ?>
+                                </span>
+                            </div>
 
                         </div>
 
