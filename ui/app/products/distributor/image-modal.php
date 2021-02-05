0eed09e89ee1371823593e0620c9f71d19f823ed
Sajad Abbasi <sajjadintel@gmail.com>
2021-02-05 23:19:32 +0200

    feature/MPW-291

--- ui/app/products/distributor/image-modal.php
+++ ui/app/products/distributor/image-modal.php
@@ -1,6 +1,6 @@
 <div id="imageModal" class="modal fade" style="padding: 100px;">
     <div class="modal-content image-input image-input-empty image-input-outline" style="width: 90%; height: 90%;">
-        <img id="imageUrl" class="image-input-wrapper" src="/theme/assets/media/users/blank.png" style="width: 100%; height: 100%;">
+        <img id="imageUrl" class="image-input-wrapper" src="/assets/img/default-product-image.png" style="width: 100%; height: 100%;">
         <label class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="change" data-toggle="tooltip" title="" data-original-title="Change avatar" onclick="DistributorProductsDataTable.closeImageModal();">
             <i class="flaticon2-cross icon-sm text-muted"></i>
         </label>
