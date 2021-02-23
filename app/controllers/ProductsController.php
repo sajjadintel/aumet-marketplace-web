1dd3a9914bbb5ce5a9cae896cac8459d13d285e4
Sajad Abbasi <sajjadintel@gmail.com>
2021-02-23 21:02:59 +0500

    Worked on MPW-499

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -2702,8 +2702,6 @@ class ProductsController extends Controller
                 $arrScientificName[] = array($scientificName['name'], $scientificName['id']);
             }
 
-            $arrScientificName = [];
-
             $dbCountry = new BaseModel($this->db, "country");
             $dbCountry->name = "name_" . $this->objUser->language;
             $allCountry = $dbCountry->findAll("name asc");
